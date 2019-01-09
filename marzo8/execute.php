f832f91eddf50037faf2ffedc513dd8de39ebef1
Giovise01 <45816513+Giovise01@users.noreply.github.com>
2019-01-09 11:33:39 +0100

    Update execute.php

--- marzo8/execute.php
+++ marzo8/execute.php
@@ -57,13 +57,13 @@
 		if($text == "barz")
 		{
 			$barz[0]="un uomo entra in un caffé, splash";
-			/*$barz[1]="sai cosa ci fa un negro su un albero? si scopa tua mamma";
-			$barz[2]="la sai la barzelletta del gay sull'albero? scendi che te la racconto";
+			$barz[1]="sai cosa ci fa un negro su un albero? si scopa tua mamma";
+			/*$barz[2]="la sai la barzelletta del gay sull'albero? scendi che te la racconto";
 			$barz[3]="la differenza fra una torta e un ebreo? il tempo di cottura";*/
 			
-			//$i=rand(0,3);
+			$i = rand(0,1);
 			
-			$parameters = array('chat_id' => $chatId, "text" => $barz[0]);
+			$parameters = array('chat_id' => $chatId, "text" => $barz[$i]);
 		}
 
 
