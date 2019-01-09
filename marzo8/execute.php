fd0a7512847fbcf66194d32fae3f771f10a51605
Giovise01 <45816513+Giovise01@users.noreply.github.com>
2019-01-09 11:31:05 +0100

    Update execute.php

--- marzo8/execute.php
+++ marzo8/execute.php
@@ -56,7 +56,7 @@
 		
 		if($text == "barz")
 		{
-			//$barz[0]="un uomo entra in un caffé, splash";
+			$barz[0]="un uomo entra in un caffé, splash";
 			/*$barz[1]="sai cosa ci fa un negro su un albero? si scopa tua mamma";
 			$barz[2]="la sai la barzelletta del gay sull'albero? scendi che te la racconto";
 			$barz[3]="la differenza fra una torta e un ebreo? il tempo di cottura";*/
