b553414869c411fcc67978ea676b66d935c48853
Giovise01 <45816513+Giovise01@users.noreply.github.com>
2019-01-09 10:51:11 +0100

    Update execute.php

--- marzo8/execute.php
+++ marzo8/execute.php
@@ -60,7 +60,7 @@
 			$berz[2]="la sai la barzelletta del gay sull'albero? scendi che te la racconto";
 			$barz[3]="la differenza fra una torta e un ebreo? il tempo di cottura";
 			$i= rand(0,3);
-			$parameters = array('chat_id' => $chatId, "text" => $text $barz[$i]);
+			$parameters = array('chat_id' => $chatId, "text" => $barz[$i]);
 		}
 		//aggiungo il comando di invio
 		//e lo invio
